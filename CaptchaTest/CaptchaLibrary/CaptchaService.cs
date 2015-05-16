e1e35bc7e7fd0aa5517980036b684ed6c5cf5dbf
arnuns <arnun.s@outlook.com>
2015-05-16 12:58:40 +0700

    Add Captch Service to Library

---
+++ CaptchaTest/CaptchaLibrary/CaptchaService.cs
@@ -0,0 +1,17 @@
+﻿
+namespace CaptchaLibrary
+{
+    public class CaptchaService
+    {
+        private IRandomNumber randomizer;
+        public CaptchaService(IRandomNumber randomizer)
+        {
+            this.randomizer = randomizer;
+        }
+        public string GetCaptcha()
+        {
+            Captcha captcha = new Captcha(randomizer.GetPattern(), randomizer.GetLeftOperand(), randomizer.GetOperator(), randomizer.GetRightOperand());
+            return captcha.ToString();
+        }
+    }
+}
